dd0d45335cd7f0a37fddfea0a72f40b147760d35
JoseAngelCabanes <joseangelcabanes29@gmail.com>
2024-05-03 15:21:15 +0200

    added event modifiers

--- app.js
+++ app.js
@@ -2,9 +2,21 @@ const app = Vue.createApp({
   data() {
     return {
       counter: 0,
+      name: '',
+      confirmedName: ''
     };
   },
   methods: {
+    confirmedInput() {
+      this.confirmedName = this.name;
+    },
+    submitForm(event) {
+      // event.preventDefault();
+      alert('Submitted!')
+    },
+    setName(event, lastName) {
+      this.name = event.target.value + ' ' + lastName;
+    },
     add(n) {
       this.counter = this.counter + n;
     },
